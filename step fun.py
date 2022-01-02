6805cd8b05eb82f748eb12ff08276e5df3cdf079
Meghana931 <86868270+Meghana931@users.noreply.github.com>
2022-01-02 22:26:10 +0530

    Add files via upload

---
+++ step fun.py
@@ -0,0 +1,9 @@
+# to create two different codes using step functions.
+# 1) 
+#range (start,stop,step):
+for items in range (0,10,3):
+    print(items)
+  
+#2) 
+for items in range (4,20,5):
+    print(items)
